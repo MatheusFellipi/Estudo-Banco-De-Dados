2f835f56ffcd3f5c5ab0c743527c1154c5940758
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-06-12 15:54:54 -0300

    Update trigger.sql

--- trigger.sql
+++ trigger.sql
@@ -76,3 +76,131 @@ INSERT INTO
   cargo (nome)
 values
   ('Teste2');
+
+/*
+ Criar um conjunto de funções engatilhadas e gatilhos que sejam capazes de atualizar o valor do 
+ atributo “produto.qtd_estoque” toda vez que uma compra ou uma venda for inserida, alterada 
+ ou excluída.
+ 
+ Dica:
+ Suponha uma inserção de uma item de venda qualquer. O seguinte código pode ser 
+ facilmente executado dentro de uma função engatilhada chamada após a inserção 
+ desse item na tabela venda_item:
+ 
+ UPDATE produto SET
+ qtd_estoque = qtd_estoque - new.qtd
+ WHERE pk_produto = new.fk_produto
+ 
+ 
+ Observe que ‘new.qtd’ representa a quantidade que um produto foi vendido e que 
+ ‘new.fk_produto’ é a chave estrangeira para o produto que foi vendido na tabela 
+ venda_item.
+ */
+CREATE
+OR REPLACE FUNCTION controle_de_estoque_venda_compra() RETURNS TRIGGER AS $ BODY $ BEGIN IF (
+  TG_OP = 'INSERT'
+  and TG_TABLE_NAME = 'comprar_item'
+) THEN
+UPDATE
+  produto
+SET
+  qtd_estoque = qtd_estoque + new.qtd
+WHERE
+  pk_produto = new.fk_produto;
+
+END IF;
+
+IF (
+  TG_OP = 'DELETE'
+  and TG_TABLE_NAME = 'comprar_item'
+) THEN
+UPDATE
+  produto
+SET
+  qtd_estoque = qtd_estoque - OLD.qtd
+WHERE
+  pk_produto = new.fk_produto;
+
+END IF;
+
+IF (
+  TG_OP = 'UPDATE'
+  and TG_TABLE_NAME = 'comprar_item'
+) THEN
+UPDATE
+  produto
+SET
+  qtd_estoque = qtd_estoque - OLD.qtd + new.qtd
+WHERE
+  pk_produto = new.fk_produto;
+
+END IF;
+
+IF (
+  TG_OP = 'INSERT'
+  and TG_TABLE_NAME = 'venda_item'
+) THEN
+UPDATE
+  produto
+SET
+  qtd_estoque = qtd_estoque - new.qtd
+WHERE
+  pk_produto = new.fk_produto;
+
+END IF;
+
+IF (
+  TG_OP = 'DELETE'
+  and TG_TABLE_NAME = 'venda_item'
+) THEN
+UPDATE
+  produto
+SET
+  qtd_estoque = qtd_estoque + OLD.qtd
+WHERE
+  pk_produto = new.fk_produto;
+
+END IF;
+
+IF (
+  TG_OP = 'UPDATE'
+  and TG_TABLE_NAME = 'venda_item'
+) THEN
+UPDATE
+  produto
+SET
+  qtd_estoque = qtd_estoque + OLD.qtd - new.qtd
+WHERE
+  pk_produto = new.fk_produto;
+
+END IF;
+
+RETURN NEW;
+
+END $ BODY $ LANGUAGE PLPGSQL;
+
+CREATE
+OR REPLACE TRIGGER controle_de_estoque
+AFTER
+INSERT
+  OR
+UPDATE
+  OR DELETE on venda_item FOR EACH ROW EXECUTE PROCEDURE controle_de_estoque();
+
+CREATE
+OR REPLACE TRIGGER controle_de_estoque
+AFTER
+INSERT
+  OR
+UPDATE
+  OR DELETE on comprar_item FOR EACH ROW EXECUTE PROCEDURE controle_de_estoque();
+
+INSERT INTO
+  venda_item (fk_venda, fk_produto, qtd, valor_unitario)
+VALUES
+  ('1', '5', '110', 10);
+
+select
+  *
+from
+  produto;
